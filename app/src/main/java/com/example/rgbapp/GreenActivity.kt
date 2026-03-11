a8639f431b4d8be4c655512becfa5fa22d1dd5bb
sara_saye <darwishsara841@gamil.com>
2026-03-11 21:35:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/rgbapp/GreenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.rgbapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class GreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_green)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val bytnblue = findViewById<Button>(R.id.bluebutton)
+        bytnblue.setOnClickListener {
+            val i = Intent(this, BlueActivity::class.java)
+            startActivity(i)
+        }
+
+    }
+}
